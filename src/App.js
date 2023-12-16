4d40943e0d5b45d81215a2ef9482030026a9a5d9
rishabh gehlot <rishabh3x@gmail.com>
2023-12-16 14:00:23 +0530

    04

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-// import logo from './logo.svg';
-// import './App.css';
-import SwipeTabs from './SwipeTabs';
+import React from "react";
+import { NavigationContainer, TabNavigator } from "@react-navigation/tabs";
 
-function App() {
+function HomeScreen() {
+  // ... your home screen content
   return (
-    <div className="App">
-      <SwipeTabs />
+    <div>
+      HomeScreen
     </div>
-  );
+  )
 }
+
+function SettingsScreen() {
+  // ... your settings screen content
+  return (
+    <div>
+      SettingsScreen
+    </div>
+  )
+}
+
+const MyTabs = () => {
+  return (
+    <TabNavigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </TabNavigator>
+  );
+};
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+};
 
 export default App;
